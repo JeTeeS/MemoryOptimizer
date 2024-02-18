832f1356709594b7377f5031c48e31ae710871d8
JeTeeS <jetees.vr@gmail.com>
2024-02-18 22:29:04 +0100

    Add deselect button

--- Editor/MemoryOptimizerWindow.cs
+++ Editor/MemoryOptimizerWindow.cs
@@ -157,6 +157,15 @@ namespace JeTeeS.MemoryOptimizer
                     {
                         EditorGUILayout.LabelField("Avatar Parameters: ", EditorStyles.boldLabel);
 
+                        if (GUILayout.Button("Deselect Prefix"))
+                        {
+                            string name = EditorInputDialog.Show("Question", "Please enter your prefix to deselect", "");
+                            if (!string.IsNullOrEmpty(name))
+                                foreach (MemoryOptimizerMain.MemoryOptimizerListData param in paramList.FindAll(x => x.param.name.StartsWith(name))) param.selected = false;
+
+                            OnChangeUpdate();
+                        }
+
                         if (GUILayout.Button("Select All"))
                         {
                             foreach (MemoryOptimizerMain.MemoryOptimizerListData param in paramList) param.selected = true;
@@ -523,6 +532,120 @@ namespace JeTeeS.MemoryOptimizer
             {
                 return new SqueezeSettings { width1 = val.Item1, width2 = val.Item2, type = val.Item3, style = val.Item4 };
             }
+        }
+
+        //https://forum.unity.com/threads/is-there-a-way-to-input-text-using-a-unity-editor-utility.473743/#post-7191802
+        public class EditorInputDialog : EditorWindow
+        {
+            string description, inputText;
+            string okButton, cancelButton;
+            bool initializedPosition = false;
+            Action onOKButton;
+
+            bool shouldClose = false;
+
+            #region OnGUI()
+            void OnGUI()
+            {
+                // Check if Esc/Return have been pressed
+                var e = Event.current;
+                if (e.type == EventType.KeyDown)
+                {
+                    switch (e.keyCode)
+                    {
+                        // Escape pressed
+                        case KeyCode.Escape:
+                            shouldClose = true;
+                            break;
+
+                        // Enter pressed
+                        case KeyCode.Return:
+                        case KeyCode.KeypadEnter:
+                            onOKButton?.Invoke();
+                            shouldClose = true;
+                            break;
+                    }
+                }
+
+                if (shouldClose)
+                {  // Close this dialog
+                    Close();
+                    //return;
+                }
+
+                // Draw our control
+                var rect = EditorGUILayout.BeginVertical();
+
+                EditorGUILayout.Space(12);
+                EditorGUILayout.LabelField(description);
+
+                EditorGUILayout.Space(8);
+                GUI.SetNextControlName("inText");
+                inputText = EditorGUILayout.TextField("", inputText);
+                GUI.FocusControl("inText");   // Focus text field
+                EditorGUILayout.Space(12);
+
+                // Draw OK / Cancel buttons
+                var r = EditorGUILayout.GetControlRect();
+                r.width /= 2;
+                if (GUI.Button(r, okButton))
+                {
+                    onOKButton?.Invoke();
+                    shouldClose = true;
+                }
+
+                r.x += r.width;
+                if (GUI.Button(r, cancelButton))
+                {
+                    inputText = null;   // Cancel - delete inputText
+                    shouldClose = true;
+                }
+
+                EditorGUILayout.Space(8);
+                EditorGUILayout.EndVertical();
+
+                // Force change size of the window
+                if (rect.width != 0 && minSize != rect.size)
+                {
+                    minSize = maxSize = rect.size;
+                }
+
+                // Set dialog position next to mouse position
+                if (!initializedPosition)
+                {
+                    var mousePos = GUIUtility.GUIToScreenPoint(Event.current.mousePosition);
+                    position = new Rect(mousePos.x + 32, mousePos.y, position.width, position.height);
+                    initializedPosition = true;
+                }
+            }
+            #endregion OnGUI()
+
+            #region Show()
+            /// <summary>
+            /// Returns text player entered, or null if player cancelled the dialog.
+            /// </summary>
+            /// <param name="title"></param>
+            /// <param name="description"></param>
+            /// <param name="inputText"></param>
+            /// <param name="okButton"></param>
+            /// <param name="cancelButton"></param>
+            /// <returns></returns>
+            public static string Show(string title, string description, string inputText, string okButton = "OK", string cancelButton = "Cancel")
+            {
+                string ret = null;
+                //var window = EditorWindow.GetWindow<InputDialog>();
+                var window = CreateInstance<EditorInputDialog>();
+                window.titleContent = new GUIContent(title);
+                window.description = description;
+                window.inputText = inputText;
+                window.okButton = okButton;
+                window.cancelButton = cancelButton;
+                window.onOKButton += () => ret = window.inputText;
+                window.ShowModal();
+
+                return ret;
+            }
+            #endregion Show()
         }
     }
 }
